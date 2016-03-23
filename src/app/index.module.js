6fc21a374afcbc56524f0b627e993065e464af74
wxl <984814106@qq.com>
2016-03-23 14:58:18 +0800

    高得地图坐标调整到中心点，自定义导出列

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,6 +5,6 @@
     .module('GPSCloud', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngResource', 'ui.router',
       'ui.bootstrap', 'toastr','highcharts-ng','ngLoadingSpinner','angular-underscore',
       'ui-notification','validation.match','angularBootstrapNavTree','ngIdle','w5c.validator',
-      'ngSanitize','ui.select','validation','ngFileUpload','ngTable','ui-select-infinity','angular-confirm','ngTableExport']);
+      'ngSanitize','ui.select','validation','ngFileUpload','ngTable','ui-select-infinity','angular-confirm']);
 
 })();
