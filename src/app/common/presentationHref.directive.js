e06a2ddbfa1c2811aefd1716986b6d95123e5018
wxl <984814106@qq.com>
2016-03-23 14:58:18 +0800

    高得地图坐标调整到中心点，自定义导出列

--- src/app/common/presentationHref.directive.js
+++ src/app/common/presentationHref.directive.js
@@ -4,9 +4,8 @@
 (function() {
   'use strict';
 
-  angular
-    .module('GPSCloud')
-    .directive('presentationHref',presentationHref)
+  var GPSCloudModule = angular.module('GPSCloud');
+    GPSCloudModule.directive('presentationHref',presentationHref);
 
   /** @ngInject */
     function presentationHref(permissions) {
@@ -28,5 +27,64 @@
          }
       }
     }
+
+ /**自定义列导出*/
+  GPSCloudModule.config(['$compileProvider', function($compileProvider) {
+    // allow data links
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|data):/);
+  }])
+   .directive('exportCsv', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            var data = '';
+            var csv = {
+                stringify: function(str) {
+                    return '"' +
+                        str.replace(/^\s\s*/, '').replace(/\s*\s$/, '') // trim spaces
+                            .replace(/"/g,'""') + // replace quotes with double quotes
+                        '"';
+                },
+                contains:function(arg,array){
+                   for(var i=0;i<array.length;i++){
+                    if(arg ==(array[i]-1)){
+                      return false;
+                    }
+                   }
+                  return true;
+                },
+                generate: function() {
+                  var args = Array.prototype.slice.call(arguments);
+                    data = '';
+                    var rows = element.find('tr');
+                    angular.forEach(rows, function(row, i) {
+                        var tr = angular.element(row),
+                            tds = tr.find('th'),
+                            rowData = '';
+                        if (tr.hasClass('ng-table-filters')) {
+                            return;
+                        }
+                        if (tds.length == 0) {
+                            tds = tr.find('td');
+                        }
+                        angular.forEach(tds, function(td, i) {
+                            if(csv.contains(i,args)){
+                                rowData += csv.stringify(angular.element(td).text()) + ';';
+                            } 
+                        });
+                        rowData = rowData.slice(0, rowData.length -1); //remove last semicolon
+                        data += rowData + "\n";
+                    });
+                },
+                link: function() {
+                    return 'data:text/csv;charset=UTF-8,' + encodeURIComponent(data);
+                }
+            };
+            $parse(attrs.exportCsv).assign(scope.$parent, csv);
+        }
+    };
+}]);
+
 
 })();
--- src/app/common/serviceResource.service.js
+++ src/app/common/serviceResource.service.js
@@ -49,7 +49,7 @@
       //实例化信息窗体
       var infoWindow = new AMap.InfoWindow({
         isCustom: true,  //使用自定义窗体
-        offset: new AMap.Pixel(105, -25)//-113, -140
+        offset: new AMap.Pixel(110, -22)//-113, -140
       });
 
       var marker = new AMap.Marker({
