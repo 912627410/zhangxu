750ef6b3b2289b9e650ed92fb9cbe7190aea7714
shuangshan.chang <shuangshan.chang@nvrchina.com.cn>
2016-05-02 17:00:08 +0800

    修改用户信息接收报警为checkbox

--- src/app/components/userManagement/addUser.controller.js
+++ src/app/components/userManagement/addUser.controller.js
@@ -14,7 +14,18 @@
     vm.operatorInfo =operatorInfo;
     //default user role for new added user
     vm.newUser = {role:"ROLE_USER"};
+
+    //默认不接收位置异常报警
+    vm.locationAlarmReceiverChk = false;
+    
     vm.ok = function (newUserinfo) {
+      if (vm.locationAlarmReceiverChk){
+        vm.newUser.locationAlarmReceiver = 1;
+      }
+      else{
+        vm.newUser.locationAlarmReceiver = 0;
+      }
+
       var restPromise = serviceResource.restAddRequest(USERINFO_URL,newUserinfo);
       restPromise.then(function (data){
         Notification.success("新建用户信息成功!");
