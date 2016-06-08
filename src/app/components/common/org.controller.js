24884a1a350513d439bb040836a46560f51f51de
wangxielong <984814106@qq.com>
2016-06-07 22:04:01 -0700

    搜索功能的初步实现

--- src/app/components/common/org.controller.js
+++ src/app/components/common/org.controller.js
@@ -2,7 +2,7 @@
  * Created by shuangshan on 16/1/19.
  */
 
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -10,22 +10,58 @@
     .controller('orgController', orgController);
 
   /** @ngInject */
-  function orgController($rootScope,$scope) {
+  function orgController($rootScope, $scope) {
     var vm = this;
     vm.selectedOrg;
-   //选中组织事件
-   vm.my_tree_handler = function(branch) {
-      $scope.$emit("OrgSelectedEvent",branch);
-     vm.selectedOrg=branch;
+    vm.searchText = '';     //搜索的数据
+    //选中组织事件
+    vm.my_tree_handler = function (branch) {
+      $scope.$emit("OrgSelectedEvent", branch);
+      vm.selectedOrg = branch;
 
-   };
+    };
 
-  //初始化组织树
-  if ($rootScope.orgChart && $rootScope.orgChart.length > 0){
-    vm.my_data=[$rootScope.orgChart[0]];
-  } else{
-    Notification.error('获取组织机构信息失败');
-  }
+    //初始化组织树
+    if ($rootScope.orgChart && $rootScope.orgChart.length > 0) {
+      vm.my_data = [$rootScope.orgChart[0]];
+    } else {
+      Notification.error('获取组织机构信息失败');
+    }
+    //TODO 搜索树
+
+    vm.search = function (searchText) {
+      //存储被搜索到的组织
+      vm.searched_array = [];
+      //展开所有的组织
+      //vm.expand_all();
+      //如果搜索为空默认生成tree
+      if (vm.searchText == '' || vm.searchText == "") {
+         vm.my_data=[$rootScope.orgChart[0]];
+      } else {
+        vm.returnSearch($rootScope.orgChart[0], vm.searchText);
+        vm.my_data=vm.searched_array;
+      }
+
+    console.log(vm.searched_array);
+
+    }
+
+    //搜索树
+    vm.returnSearch = function (orgTree, searchText) {
+      if (orgTree.label.indexOf(searchText) != -1) {
+        vm.searched_array.push(orgTree);
+
+      }
+      
+      if (orgTree.children.length != 0) {
+       angular.forEach(orgTree.children, function (node,index) {
+          vm.returnSearch(node, searchText);
+        });
+      } else {
+        vm.my_data= vm.searched_array;
+      }
+
+    };
 
   }
 })();
--- src/app/components/common/tree.controller.js
+++ src/app/components/common/tree.controller.js
@@ -1,40 +1,34 @@
-
-(function() {
+(function () {
   'use strict';
   angular
     .module('GPSCloud')
     .controller('treeController', treeController);
 
-	function treeController($rootScope,$scope,$timeout,$confirm,$filter,$uibModalInstance,languages,serviceResource,org){
-		var vm = this;
-    vm.showOrgTree = true;
+  function treeController($rootScope, $scope, $timeout, $confirm, $filter, $uibModalInstance, languages, serviceResource, org) {
+    var vm = this;
+    vm.showOrgTree = true;    //默认显示树
     vm.org = {label: ""};    //调拨组织
-    vm.searchText='';
-    vm.selectedOrg;
-
-		//关闭选择org的页面
-    vm.cancel = function () {
-      vm.select_branch();
-	     $uibModalInstance.dismiss('cancel');
-    };
+    vm.selectedOrg;         //选中的组织
 
     //响应的选中事件
     $scope.$on('OrgSelectedEvent', function (event, data) {
-       vm.selectedOrg = data;
-       vm.org  = vm.selectedOrg;
-     });
+      vm.selectedOrg = data;
+      vm.org = vm.selectedOrg;
+    });
+
+    //关闭选择org的页面
+    vm.cancel = function () {
+      vm.select_branch();
+      $uibModalInstance.dismiss('cancel');
+    };
 
     //确定
-    vm.confirm=function () {
-      $rootScope.$emit("orgSelected",vm.org);
+    vm.confirm = function () {
+      $rootScope.$emit("orgSelected", vm.org);
       //取消选中
       vm.select_branch();
       //关闭modal
       $uibModalInstance.dismiss('cancel');
-    }
-    //TODO 搜索树
-    vm.search=function(searchText){
-      console.log(searchText);
     }
 
   }
