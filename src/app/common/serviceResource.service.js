0e1caf156c752752afead35cb20f049742fc6600
sylong <yalong.shang@nvr-china.com>
2017-03-29 09:26:52 +0800

    优化代码

--- src/app/common/serviceResource.service.js
+++ src/app/common/serviceResource.service.js
@@ -229,33 +229,26 @@
         var deviceinfoPromis = restCallService(singlUrl, "GET");
         deviceinfoPromis.then(function (data) {
             $rootScope.deviceinfoMonitor = data.content;
+            var templateUrl, controller;
             if($rootScope.deviceinfoMonitor.versionNum == 'A001') {
-              $rootScope.currentOpenModal = $uibModal.open({
-                animation: true,
-                backdrop: false,
-                templateUrl: 'app/components/deviceMonitor/deviceAerialCurrentInfo.html',
-                controller: 'deviceAerialCurrentInfoController as deviceAerialCurrentInfoController',
-                size: 'lg',
-                resolve: { //用来向controller传数据
-                  deviceinfo: function () {
-                    return $rootScope.deviceinfoMonitor;
-                  }
-                } 
-              });
+              templateUrl = 'app/components/deviceMonitor/deviceAerialCurrentInfo.html';
+              controller = 'deviceAerialCurrentInfoController as deviceAerialCurrentInfoController';
             } else {
-              $rootScope.currentOpenModal = $uibModal.open({
-                animation: true,
-                backdrop: false,
-                templateUrl: 'app/components/deviceMonitor/devicecurrentinfo.html',
-                controller: 'DeviceCurrentInfoController as deviceCurrentInfoCtrl',
-                size: 'lg',
-                resolve: { //用来向controller传数据
-                  deviceinfo: function () {
-                    return $rootScope.deviceinfoMonitor;
-                  }
-                }
-              });
+              templateUrl = 'app/components/deviceMonitor/devicecurrentinfo.html';
+              controller = 'DeviceCurrentInfoController as deviceCurrentInfoCtrl';
             }
+            $rootScope.currentOpenModal = $uibModal.open({
+              animation: true,
+              backdrop: false,
+              templateUrl: templateUrl,
+              controller: controller,
+              size: 'lg',
+              resolve: { //用来向controller传数据
+                deviceinfo: function () {
+                  return $rootScope.deviceinfoMonitor;
+                }
+              }
+            });
 
           }, function (reason) {
             Notification.error(languages.findKey('failedToGetDeviceInformation'));
