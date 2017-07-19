368f500f4b672e0b4597aa0470472725e3e65070
fengxiaopeng <744811621@qq.com>
2017-07-19 17:01:27 +0800

    修改高空车远程控制、电子围栏

--- src/app/components/privilegeManagement/newMenu.controller.js
+++ src/app/components/privilegeManagement/newMenu.controller.js
@@ -9,30 +9,29 @@
     .controller('newMenuController', newMenuController);
 
   /** @ngInject */
-  function newMenuController($scope, $uibModalInstance,PRIVILAGE_OPER_URL, serviceResource, Notification, parentMenu) {
+  function newMenuController($scope, $uibModalInstance, serviceResource, MENU_URL,Notification, parentMenu) {
     var vm = this;
     vm.operatorInfo = $scope.userInfo;
     vm.parentMenu = parentMenu;
-    vm.priviligeInfo = {};
+    vm.menu = {parentId: vm.parentMenu.id};
 
+      vm.ok = function (menu) {
 
-    vm.ok = function (priviligeInfo) {
-     var restPromise = serviceResource.restAddRequest(PRIVILAGE_OPER_URL, priviligeInfo);
-      restPromise.then(function (data) {
-        if(data.code===0){
-          Notification.success("新建权限信息成功!");
-          $uibModalInstance.close(data.content);
-        }else{
-          vm.priviligeInfo = priviligeInfo;
-          Notification.error(data.message);
+       var restPromise = serviceResource.restAddRequest(MENU_URL, menu);
+        restPromise.then(function (data) {
+          if(data.code===0){
+            Notification.success("新建菜单信息成功!");
+            $uibModalInstance.close(data.content);
+          }else{
+            Notification.error(data.message);
+          }
+        }, function (reason) {
+            vm.errorMsg=reason.data.message;
+            Notification.error(reason.data.message);
         }
-      }, function (reason) {
-          vm.errorMsg=reason.data.message;
-          Notification.error(reason.data.message);
-      }
 
-      );
-    };
+        );
+      };
 
     vm.cancel = function () {
       $uibModalInstance.dismiss('cancel');
