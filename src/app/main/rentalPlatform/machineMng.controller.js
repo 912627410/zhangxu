d832bcac44069f4253654b34b0fee772a1dc6f91
xielongwang <984814106@qq.com>
2017-07-26 08:54:59 +0800

    租赁车队管理页面布局效果

--- src/app/main/rentalPlatform/machineMng.controller.js
+++ src/app/main/rentalPlatform/machineMng.controller.js
@@ -11,8 +11,24 @@
 
   /** @ngInject */
 
-  function rentalMachineMngController($scope, $translate) {
+  function rentalMachineMngController($scope, $location,$anchorScroll) {
     var vm = this;
+    $anchorScroll.yOffset = 50;
+    //定义页面的喵点
+    vm.anchorList=["currentLocation","currentState","alarmInfo","historyData","liftCycle"];
+    //用户名
+    vm.userName="Admin";
+
+
+    vm.gotoAnchor = function(x) {
+      var newHash = 'anchor' + x;
+      if ($location.hash() !== newHash) {
+        $location.hash('anchor' + x);
+      } else {
+        $anchorScroll();
+      }
+    };
+
 
 
   }
