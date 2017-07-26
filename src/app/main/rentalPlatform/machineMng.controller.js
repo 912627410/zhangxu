62ce4d84a3a401beee5e1bebff472bc882be998d
xielongwang <984814106@qq.com>
2017-07-26 14:42:08 +0800

    页面布局调整

--- src/app/main/rentalPlatform/machineMng.controller.js
+++ src/app/main/rentalPlatform/machineMng.controller.js
@@ -8,27 +8,34 @@
     .module('GPSCloud')
     .controller('rentalMachineMngController', rentalMachineMngController);
 
-
   /** @ngInject */
-
-  function rentalMachineMngController($scope, $location,$anchorScroll) {
+  function rentalMachineMngController($scope, $window, $location, $anchorScroll, serviceResource) {
     var vm = this;
+    //定义偏移量
     $anchorScroll.yOffset = 50;
     //定义页面的喵点
-    vm.anchorList=["currentLocation","currentState","alarmInfo","historyData","liftCycle"];
-    //用户名
-    vm.userName="Admin";
-
-
-    vm.gotoAnchor = function(x) {
-      var newHash = 'anchor' + x;
-      if ($location.hash() !== newHash) {
-        $location.hash('anchor' + x);
-      } else {
-        $anchorScroll();
-      }
+    vm.anchorList = ["currentLocation", "currentState", "alarmInfo"];
+    //自适应高度
+    var windowHeight = $window.innerHeight; //获取窗口高度
+    windowHeight = windowHeight - 50 - 15 - 90 - 15 - 5;//50 topBar的高,15间距,90msgBox高,15间距,5 预留
+    var leftMapBoxHeight = windowHeight / 4;
+    vm.mapBox = {
+      "min-height": windowHeight + "px"
+    }
+    vm.leftMapBox = {
+      "min-height": leftMapBoxHeight + "px",
+      "width": "90%"
+    }
+    //初始化地图
+    serviceResource.refreshMapWithDeviceInfo("homeMap", null, 4);
+    /**
+     * 去到某个喵点
+     * @param 喵点id
+     */
+    vm.gotoAnchor = function (x) {
+      $location.hash(x);
+      $anchorScroll();
     };
-
 
 
   }
