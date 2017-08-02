1603289d5e507c56627a1edaa9ceb1dbcf83ffc9
xielongwang <984814106@qq.com>
2017-08-02 17:40:29 +0800

    历史数据页面

--- src/app/components/rentalPlatform/machineMng/machineHistoryData.controller.js
+++ src/app/components/rentalPlatform/machineMng/machineHistoryData.controller.js
@@ -9,7 +9,24 @@
     .controller('machineHistoryDataController', machineHistoryDataController);
 
   /** @ngInject */
-  function machineHistoryDataController($scope, $window, $location) {
+  function machineHistoryDataController($scope, $window, $location, $anchorScroll, NgTableParams, ngTableDefaults, serviceResource) {
+    var vm =this;
+    vm.current = "A";
+    //自适应高度
+    var windowHeight = $window.innerHeight; //获取窗口高度
+    function adjustWindow(windowHeight) {
+      var baseBoxContainerHeight = windowHeight - 50 - 15 - 90 - 15 - 7;//50 topBar的高,15间距,90msgBox高,15间距,8 预留
+      //baseBox自适应高度
+      vm.baseBoxContainer = {
+        "min-height": baseBoxContainerHeight + "px"
+      }
+    }
+    /**
+     * 监听窗口大小改变后重新自适应高度
+     */
+    $scope.$watch('height', function(old, newv){
+      adjustWindow(newv);
+    })
 
   }
 })();
