f778ccf8b5a87ecb9edfd5142da7f9b8092107ec
xielongwang <984814106@qq.com>
2017-08-14 18:40:08 +0800

    地图圆圈大小

--- src/app/common/serviceResource.service.js
+++ src/app/common/serviceResource.service.js
@@ -249,15 +249,49 @@
      * @param markers
      */
     function aggregationShow(map, markers) {
-      var sts = [{
+    /*  var sts = [{
         url: "http://a.amap.com/jsapi_demos/static/images/blue.png",
         size: new AMap.Size(32, 32),
         offset: new AMap.Pixel(-16, -16)
       }];
-      new AMap.MarkerClusterer(map, markers, {
+      var point = new AMap.MarkerClusterer(map, markers, {
         styles: sts,
         gridSize: 40
+      });*/
+      var count  = markers.length;
+      var _renderCluserMarker = function (context) {
+        var factor = Math.pow(context.count/count,1/18)
+        var div = document.createElement('div');
+        var Hue = 180 - factor* 180;
+        var bgColor = 'hsla('+Hue+',100%,50%,0.7)';
+        var fontColor = 'hsla('+Hue+',100%,20%,1)';
+        var borderColor = 'hsla('+Hue+',100%,40%,1)';
+        var shadowColor = 'hsla('+Hue+',100%,50%,1)';
+        div.style.backgroundColor = bgColor
+        if (context.count>100){
+          var size = Math.round(30 + Math.pow(context.count/count,1/5) * 20)+20;
+
+        }else {
+          var size = Math.round(30 + Math.pow(context.count/count,1/5) * 20);
+        }
+        div.style.width = div.style.height = size+'px';
+        div.style.border = 'solid 1px '+ borderColor;
+        div.style.borderRadius = size/2 + 'px';
+        div.style.boxShadow = '0 0 1px '+ shadowColor;
+        div.innerHTML = context.count;
+        div.style.lineHeight = size+'px';
+        div.style.color = fontColor;
+        div.style.fontSize = '14px';
+        div.style.textAlign = 'center';
+        context.marker.setOffset(new AMap.Pixel(-size/2,-size/2));
+        context.marker.setContent(div)
+      }
+
+      var cluster = new AMap.MarkerClusterer(map,markers,{
+        gridSize:80,
+        renderCluserMarker:_renderCluserMarker
       });
+
     }
 
     return {
