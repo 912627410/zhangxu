2b99ca26c72bd0947f51edabf0ccdf9c6002f148
xielongwang <984814106@qq.com>
2017-09-14 16:26:26 +0800

    报警消息处理

--- src/app/components/rentalPlatform/machineMng/machineAlarmInfo.controller.js
+++ src/app/components/rentalPlatform/machineMng/machineAlarmInfo.controller.js
@@ -274,6 +274,14 @@
 
       $rootScope.currentOpenModal.result.then(function (newVal) {
         //数据
+        var tabList=vm.notificationList;
+        //更新内容
+        for(var i=0;i<tabList.length;i++){
+          if(tabList[i].id==newVal.id){
+            tabList[i].processStatus=newVal.processStatus;
+            break;
+          }
+        }
       }, function () {
         //取消
       });
--- src/app/components/rentalPlatform/machineMng/processAlarmInfo.controller.js
+++ src/app/components/rentalPlatform/machineMng/processAlarmInfo.controller.js
@@ -9,15 +9,17 @@
     .controller('processAlarmInfoController', processAlarmInfoController);
 
   /** @ngInject */
-  function processAlarmInfoController($rootScope, $scope, $window, $location,notification,notifications,$uibModalInstance, $anchorScroll, $uibModal, serviceResource, languages, commonFactory,RENTAL_PRCESS_ALARM,RENTAL_PRCESS_ALARMS) {
+  function processAlarmInfoController($rootScope, $scope, $window, $location, notification, notifications, $uibModalInstance, $anchorScroll, $uibModal,Notification, serviceResource, languages, commonFactory, RENTAL_PRCESS_ALARM, RENTAL_PRCESS_ALARMS) {
     var vm = this;
     var userInfo = $rootScope.userInfo;
-    vm.notificationProcess={};
+    var emailValidator = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    var telephoneNoValidator = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+    vm.notificationProcess = {};
 
     /**
      * 关闭对话框
      */
-    vm.cancel=function () {
+    vm.cancel = function () {
       $uibModalInstance.dismiss('cancel');
     }
 
@@ -25,19 +27,24 @@
     /**
      * 报警消息处理
      */
-    vm.processAlarm=function () {
-      var notificationProcessCopy={};
-      notificationProcessCopy.processUser=userInfo.userdto.ssn;
+    vm.processAlarm = function () {
+      if (!vm.validator(vm.notificationProcess.receiver)) {
+        Notification.error("请输入正确邮箱或者手机号多个使用逗号隔开!");
+        return;
+      }
+      var notificationProcessCopy = {};
+      notificationProcessCopy.processUser = userInfo.userdto.ssn;
       //判断处理单个消息
-      if (notification!=null && notifications==null){
+      if (notification != null && notifications == null) {
+        vm.notificationProcess.alarmMsgId=notification.id;
         angular.extend(notificationProcessCopy, notification, vm.notificationProcess);
         vm.processSingeAlarm(notificationProcessCopy);
         return;
       }
 
       //处理多个消息
-      angular.extend(notificationProcessCopy,vm.notificationProcess, notificationProcessCopy);
-      notificationProcessCopy.notifications=notifications;
+      angular.extend(notificationProcessCopy, vm.notificationProcess, notificationProcessCopy);
+      notificationProcessCopy.notifications = notifications;
       vm.processMultipleAlarm(notificationProcessCopy);
     }
 
@@ -45,25 +52,46 @@
      * 处理单个报警消息
      * @param notificationProcess
      */
-    vm.processSingeAlarm=function (notificationProcess) {
-      var processPromis = serviceResource.restCallService(RENTAL_PRCESS_ALARM,"ADD",notificationProcess);
+    vm.processSingeAlarm = function (notificationProcess) {
+      var processPromis = serviceResource.restCallService(RENTAL_PRCESS_ALARM, "ADD", notificationProcess);
       processPromis.then(function (data) {
-
-      },function (reason) {
+        if (data.content==true){
+          notification.processStatus=true;
+          $uibModalInstance.close(notification);
+        }
+      }, function (reason) {
 
       })
     }
 
     /**
-     *
+     *处理多个报警信息
      */
-    vm.processMultipleAlarm=function (notifications) {
-      var processPromis = serviceResource.restCallService(RENTAL_PRCESS_ALARMS,"ADD",notifications);
+    vm.processMultipleAlarm = function (notifications) {
+      var processPromis = serviceResource.restCallService(RENTAL_PRCESS_ALARMS, "ADD", notifications);
       processPromis.then(function (data) {
 
-      },function (reason) {
+      }, function (reason) {
 
       })
     }
+
+    /**
+     *验证邮箱和手机号
+     */
+    vm.validator = function (emailOrPhone) {
+      if (emailOrPhone == null || emailOrPhone == undefined) {
+        return false;
+      }
+      var smsArr = emailOrPhone.split(",");
+      for (var index in smsArr) {
+        if (!emailValidator.test(smsArr[index]) && !telephoneNoValidator.test(smsArr[index])) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+
   }
 })();
