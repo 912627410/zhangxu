afb79d3027f1ac1579350788ddf63c7148d68776
xielongwang <984814106@qq.com>
2017-10-25 10:24:11 +0800

    增加监控数据页面

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -316,6 +316,7 @@
     .constant('RENTAL_LOCUS_DATA',SERVER_BASE_URL+'rental/device-locus-data')//定位历史数据
     .constant('RENTAL_TRAJECTORY_DATA',SERVER_BASE_URL+'rental/trajectory')//定位历史数据
     .constant('RENTAL_DEVCE_DATA_PAGED_QUERY',SERVER_BASE_URL+'rental/deviceHistoryData')//定位历史数据
+    .constant('RENTAL_DEVCE_MONITOR_DATA',SERVER_BASE_URL+'rental/monitor')//设备监控(数据)
 
 
     //租赁客户信息
