b9df7fdf6a6c181487c2249f03b95b36bc0d42e9
yibo.chen <yibo.chen@nvrchina.com.cn>
2017-12-20 18:05:57 +0800

    进场单附件上传“

--- src/app/components/rentalPlatform/fleetMng/rentalGoSite.js
+++ src/app/components/rentalPlatform/fleetMng/rentalGoSite.js
@@ -1,4 +1,3 @@
-
 (function () {
   'use strict';
 
@@ -6,16 +5,16 @@
     .module('GPSCloud')
     .controller('rentalGoSiteController', rentalGoSiteController);
 
-  function rentalGoSiteController($rootScope,$uibModalInstance,$stateParams,ngTableDefaults,NgTableParams,serviceResource,treeFactory,rentalOrder,commonFactory,
-                                  rentalService,DEFAULT_SIZE_PER_PAGE,RENTANL_ORDER_MACHINE_BATCH_MOVE_URL,RENTANL_UNUSED_MACHINE_PAGE_URL,Notification,languages) {
-    var vm=this;
+  function rentalGoSiteController($rootScope, $uibModalInstance, $stateParams, ngTableDefaults, NgTableParams, serviceResource, treeFactory, rentalOrder, commonFactory,
+                                  rentalService, DEFAULT_SIZE_PER_PAGE, RENTANL_ORDER_MACHINE_BATCH_MOVE_URL, Upload, RENTANL_UNUSED_MACHINE_PAGE_URL, Notification, languages) {
+    var vm = this;
     vm.userInfo = $rootScope.userInfo;
-    vm.selectAll=false;
+    vm.selectAll = false;
     vm.rentalOrder = rentalOrder
-    vm.selected=[];
+    vm.selected = [];
     vm.pageSize = 8;
-    var date=new Date();
-    vm.goSiteDate=date;
+    var date = new Date();
+    vm.goSiteDate = date;
     vm.goSiteDateOpenStatusData = {
       opened: false
     };
@@ -28,20 +27,20 @@
       formatYear: 'yyyy',
       startingDay: 1
     };
-    ngTableDefaults.params.count=DEFAULT_SIZE_PER_PAGE;
-    ngTableDefaults.settings.counts=[];
+    ngTableDefaults.params.count = DEFAULT_SIZE_PER_PAGE;
+    ngTableDefaults.settings.counts = [];
 
     //组织树的显示
-    vm.openTreeInfo=function() {
+    vm.openTreeInfo = function () {
       treeFactory.treeShow(function (selectedItem) {
-        vm.org =selectedItem;
+        vm.org = selectedItem;
       });
     }
 
     //加载品牌信息
     var deviceManufactureListPromise = rentalService.getDeviceManufactureList();
     deviceManufactureListPromise.then(function (data) {
-      vm.deviceManufactureList= data.content;
+      vm.deviceManufactureList = data.content;
       //    console.log(vm.userinfoStatusList);
     }, function (reason) {
       Notification.error(languages.findKey('getVendorFail'));
@@ -50,7 +49,7 @@
     //加载高度信息
     var deviceHeightTypeListPromise = rentalService.getDeviceHeightTypeList();
     deviceHeightTypeListPromise.then(function (data) {
-      vm.deviceHeightTypeList= data.content;
+      vm.deviceHeightTypeList = data.content;
     }, function (reason) {
       Notification.error(languages.findKey('getHeFail'));
     })
@@ -58,7 +57,7 @@
     //加载车辆类型信息
     var deviceTypeListPromise = rentalService.getDeviceTypeList();
     deviceTypeListPromise.then(function (data) {
-      vm.deviceTypeList= data.content;
+      vm.deviceTypeList = data.content;
     }, function (reason) {
       Notification.error(languages.findKey('getTypeFail'));
     })
@@ -66,25 +65,52 @@
     //加载车辆驱动信息
     var devicePowerTypeListPromise = rentalService.getDevicePowerTypeList();
     devicePowerTypeListPromise.then(function (data) {
-      vm.devicePowerTypeList= data.content;
+      vm.devicePowerTypeList = data.content;
     }, function (reason) {
       Notification.error(languages.findKey('getDriFail'));
     })
 
     //批量进场（批量调入）
-    vm.batchMoveMachine = function () {
+    vm.batchMoveMachine = function (file) {
+      /*        var rentalOrderMachineOperVo = {"addMachineIdList": vm.selected, "orderId": vm.rentalOrder.id,"operationType":2,"recordTime":vm.goSiteDate};
+              var restPromise = serviceResource.restUpdateRequest(RENTANL_ORDER_MACHINE_BATCH_MOVE_URL, rentalOrderMachineOperVo);
+              restPromise.then(function (data) {
+                if(data.code==0){
+                  Notification.success(languages.findKey('transVehicle'));
 
-        var rentalOrderMachineOperVo = {"addMachineIdList": vm.selected, "orderId": vm.rentalOrder.id,"operationType":2,"recordTime":vm.goSiteDate};
-        var restPromise = serviceResource.restUpdateRequest(RENTANL_ORDER_MACHINE_BATCH_MOVE_URL, rentalOrderMachineOperVo);
-        restPromise.then(function (data) {
-          if(data.code==0){
-            Notification.success(languages.findKey('transVehicle'));
+                  }
+                }, function (reason) {
+                  Notification.error(languages.findKey('transVehiclFail'));
+                });*/
 
-            }
-          }, function (reason) {
-            Notification.error(languages.findKey('transVehiclFail'));
-          });
-        $uibModalInstance.close(vm.selected.length);
+      var recordTime = serviceResource.getChangeChinaTime(vm.goSiteDate);
+      var startMonth = recordTime.getMonth() + 1;  //getMonth返回的是0-11
+      recordTime = recordTime.getFullYear() + '-' + startMonth + '-' + recordTime.getDate() + ' ' + recordTime.getHours() + ':' + recordTime.getMinutes() + ':' + recordTime.getSeconds();
+
+      var uploadurl = RENTANL_ORDER_MACHINE_BATCH_MOVE_URL;
+      uploadurl += "?orderId=" + vm.rentalOrder.id + "&operationType=" + 2 + "&machineIdList=" + vm.selected + "&recordTime=" + recordTime
+      file.upload = Upload.upload({
+        url: uploadurl,
+        file: file
+      });
+      file.upload.then(function (response) {
+        $timeout(function () {
+          file.result = response.data;
+          if (file.result.code == 0) {
+            Notification.success("新增文件成功!");
+            $uibModalInstance.close();
+          } else {
+            Notification.error(data.message);
+          }
+        })
+      }, function (reason) {
+        vm.errorMsg = reason.data.message;
+        Notification.error("新增文件失败!");
+        Notification.error(vm.errorMsg);
+      }, function (evt) {
+      });
+
+      $uibModalInstance.close(vm.selected.length);
 
     };
 
@@ -97,7 +123,7 @@
       vm.org = null;
     }
 
-    vm.queryMachine = function (searchConditions,page,size,sort) {
+    vm.queryMachine = function (searchConditions, page, size, sort) {
       var restCallURL = RENTANL_UNUSED_MACHINE_PAGE_URL;
       var pageUrl = page || 0;
       var sizeUrl = size || 8;
@@ -105,33 +131,32 @@
       restCallURL += "?page=" + pageUrl + '&size=' + sizeUrl + '&sort=' + sortUrl;
 
       restCallURL += "&orgId=" + vm.userInfo.userdto.organizationDto.id;
-      if (searchConditions!=null){
-        if(searchConditions.licenseId!=null){
+      if (searchConditions != null) {
+        if (searchConditions.licenseId != null) {
           restCallURL += "&licenseId=" + searchConditions.licenseId;
         }
-        if(searchConditions.type!=null){
+        if (searchConditions.type != null) {
           restCallURL += "&typeId=" + searchConditions.type.id;
         }
-        if(searchConditions.powerType!=null){
+        if (searchConditions.powerType != null) {
           restCallURL += "&powerTypeId=" + searchConditions.powerType.id;
         }
-        if(searchConditions.heightType!=null){
+        if (searchConditions.heightType != null) {
           restCallURL += "&heightTypeId=" + searchConditions.heightType.id;
         }
 
-        if(searchConditions.factory!=null){
+        if (searchConditions.factory != null) {
           restCallURL += "&factoryId=" + searchConditions.factory.id;
         }
-        if(searchConditions.address!=null){
+        if (searchConditions.address != null) {
           restCallURL += "&address=" + searchConditions.address;
         }
 
 
       }
-        if (null != vm.org&&null != vm.org.id&&!vm.querySubOrg) {
-             restCallURL += "&orgId=" + vm.org.id;
-        }
-
+      if (null != vm.org && null != vm.org.id && !vm.querySubOrg) {
+        restCallURL += "&orgId=" + vm.org.id;
+      }
 
 
       var rspData = serviceResource.restCallService(restCallURL, "GET");
@@ -154,7 +179,7 @@
       });
 
     }
-    vm.queryMachine(null,null,null,null,null);
+    vm.queryMachine(null, null, null, null, null);
 
     var updateSelected = function (action, machine) {
       if (action == 'add' && vm.selected.indexOf(machine.id) == -1) {
